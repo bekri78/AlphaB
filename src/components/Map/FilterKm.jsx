e802944b9204fbf7264a21331b071de6baa878a2
Emeline <leduc.emeline44@gmail.com>
2021-04-25 20:15:17 +0200

    add filter km

---
+++ src/components/Map/FilterKm.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+  root: {
+    width: 150,
+  },
+});
+
+const marks = [
+  {
+    value: 1,
+    label: '1',
+  },
+  {
+    value: 5,
+    label: '5',
+  },
+  {
+    value: 10,
+    label: '10',
+  },
+  {
+    value: 15,
+    label: '15',
+  },
+];
+
+function valuetext(value) {
+  return `${value}km`;
+}
+
+function FilterKm(props) {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const handleChangeValue = (event, newValue) => {
+    setValue(newValue);
+    // eslint-disable-next-line react/prop-types
+    props.changeRadius(newValue);
+  };
+  console.log(value);
+
+  return (
+    <div className={classes.root}>
+      <Typography id="discrete-slider" gutterBottom>
+        Distance en km
+      </Typography>
+      <Slider
+        defaultValue={value}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={1}
+        marks={marks}
+        min={0}
+        max={15}
+        value={value}
+        onChange={handleChangeValue}
+      />
+    </div>
+  );
+}
+
+export default FilterKm;
