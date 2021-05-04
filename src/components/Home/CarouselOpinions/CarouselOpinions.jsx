46fd14f0b305e551a46bf747e2406f6066a1106d
Emeline <leduc.emeline44@gmail.com>
2021-05-04 20:29:27 +0200

    add first content in rating card with axios

--- src/components/Home/CarouselOpinions/CarouselOpinions.jsx
+++ src/components/Home/CarouselOpinions/CarouselOpinions.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import Swiper core and required modules
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { EffectCoverflow, Pagination } from 'swiper/core';
+import axios from 'axios';
 import 'swiper/swiper.min.css';
 import 'swiper/components/effect-coverflow/effect-coverflow.min.css';
 import 'swiper/components/pagination/pagination.min.css';
@@ -11,6 +12,19 @@ import './CarouselOpinions.css';
 SwiperCore.use([EffectCoverflow, Pagination]);
 
 function CarouselOpinions() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/ratings')
+      .then((response) => response.data)
+      .then((data) => {
+        setResults(data);
+      });
+  }, []);
+
+  console.log(results);
+
   return (
     <div className="opinion-container">
       <Swiper
@@ -30,13 +44,10 @@ function CarouselOpinions() {
         <SwiperSlide className="card-opinion">
           <div className="card-content">
             <div className="layer"></div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lectus enim, gravida at risus vel, fermentum gravida lorem. Aenean ac
-              blandit quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+            <p>{results[0].message}</p>
             <div className="card-user">
-              <div className="card-user-name">Anne-Laure</div>
-              <div className="card-user-job">Développeuse</div>
+              <div className="card-user-name">{`${results[0].firstname} ${results[0].lastname.charAt(0)}.`}</div>
+              <div className="card-user-job">{results[0].job}</div>
             </div>
           </div>
         </SwiperSlide>
