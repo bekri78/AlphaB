21f8ddde4db384f8381a79b59344974e8d5d1489
mehdi <bekri.mehdi@outlook.com>
2021-05-20 00:06:44 +0200

    add style login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Navigation from './components/Navbar/navbar';
+import Home from './components/Home/Home';
+import TextToSpeech from './views/TextToSpeech';
+import CardMaps from './components/Map/CardMaps';
+import SimpleAccordion from './components/Questions/questions';
+import Contact from './components/Contact/Contact';
+import logo from './components/Navbar/img/logo.png';
+import ScrollReveal from './components/ScrollReveal/ScrollReveal';
 import firebase from './utils/firebaseConfig';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { Route, Switch } from 'react-router-dom';
-import Home from './components/Home/Home';
-import SimpleAccordion from './components/Questions/questions';
-import TextToSpeech from './views/TextToSpeech';
-import CardMaps from './components/Map/CardMaps';
-import Contact from './components/Contact/Contact';
-
 import './App.css';
-import Navigation from './components/Navbar/navbar';
 function App() {
   const [isSignedIn, setSignedIn] = useState(false);
   const uiConfig = {
@@ -41,9 +42,14 @@ function App() {
           </Switch>
         </>
       ) : (
-        <div>
-          <h1>react</h1>
+        <div style={{ backgroundColor: '#1b2437' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', paddingTop: '50px' }}>
+            <img src={logo} alt="logo" id="logoConnexion" />
+            <p style={{ paddingTop: '2%', color: 'white', fontSize: 'large', marginTop: 'auto' }}>Connexion</p>
+          </div>
+
           <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+          <ScrollReveal />
         </div>
       )}
     </div>
