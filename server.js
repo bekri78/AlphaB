c0ee8be0d47a2c4f5127a6e087b82b196cbb4669
BEKRI Mehdi <bekri78>
2021-06-12 10:31:46 +0200

    add server

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+// const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 8080;
+
+app.post("/payment", cors(), async (req, res) => {
+  let { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "premium abonnement",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("payment", payment);
+    res.json({
+      message: "paymement success full",
+      success: true,
+    });
+  } catch (error) {
+    console.log("error", error);
+    res.json({
+      message: "payment failed",
+      success: false,
+    });
+  }
+});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("my-app/build"));
+}
+app.listen(PORT, (err) => {
+  if (err) return console.log(err);
+  console.log(` server listen on port ${PORT} `);
+});
